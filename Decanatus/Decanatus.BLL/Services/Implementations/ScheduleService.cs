0244d9d0855f207b835e543d3166179f63e4095d
Dima <didiakivskyy@gmail.com>
2022-04-28 23:08:17 +0300

    fix bug

--- Decanatus/Decanatus.BLL/Services/Implementations/ScheduleService.cs
+++ Decanatus/Decanatus.BLL/Services/Implementations/ScheduleService.cs
@@ -12,7 +12,7 @@ namespace Decanatus.BLL.Services
 
         private Func<IQueryable<Lesson>, IIncludableQueryable<Lesson, object>> GetInclude()
         {
-            Func<IQueryable<Lesson>, IIncludableQueryable<Lesson, object>> expr = x => x.Include(i => i.Audience).Include(c => c.Subject).Include(a => a.Lecturers).Include(b => b.Groups);
+            Func<IQueryable<Lesson>, IIncludableQueryable<Lesson, object>> expr = x => x.Include(i => i.Audience).Include(c => c.Subject).Include(a => a.Lecturers).Include(b => b.Groups).Include(b => b.LessonNumber);
             return expr;
         }
 
