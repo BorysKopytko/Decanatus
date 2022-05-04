bdd9d14b85b7c0076b753579bfae0bc4480a5896
Borys Kopytko <kopytkoborys@gmail.com>
2022-05-04 19:11:03 +0300

    Add Create and Delete action for Schedule for Administrator

--- Decanatus/Decanatus.DAL/Models/LessonGroup.cs
+++ Decanatus/Decanatus.DAL/Models/LessonGroup.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 
 namespace Decanatus.DAL.Models
 {
-    public class LessonGroup :Entity
+    public class LessonGroup : Entity
     {
         public int LessonId { get; set; }
         public int GroupId { get; set; }
-
         public Lesson Lesson { get; set; }
         public Group Group { get; set; }    
     }
