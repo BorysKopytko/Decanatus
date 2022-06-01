c3f5eae637eb7d1a38ef2b6847c30daf102b2df0
SwaTiK4931 <40461618+SwaTiK4931@users.noreply.github.com>
2022-06-01 21:07:38 +0300

    fixed home page

--- Decanatus/Decanatus.Web/Controllers/HomeController.cs
+++ Decanatus/Decanatus.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Decanatus.BLL.Services.Interfaces;
+using Decanatus.DAL.Models;
 using Decanatus.Web.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -10,15 +12,22 @@ namespace Decanatus.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IHomeService _homeService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(IHomeService homeService)
+        public HomeController(IHomeService homeService, UserManager<ApplicationUser> userManager)
         {
             _homeService = homeService;
+            _userManager = userManager;
         }
 
-        public IActionResult Index(int id = 1)
+        public IActionResult Index()
         {
-            return View(_homeService.GetStudent(id));
+            if (_userManager.GetRolesAsync(_userManager.Users.FirstOrDefault(user => user.Id == _userManager.GetUserId(User))).Result.Contains("Студент"))
+            {
+                return View(_homeService.GetStudent(_userManager.GetUserAsync(User).Result.PersonId));
+            }
+
+            return View(null);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
